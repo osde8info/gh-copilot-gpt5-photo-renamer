6eb68a29e2011ea2f6c2162529c8a5986a213b4d
clive darra <cdarra@github.com>
2026-01-07 16:20:47 +0000

    Initial Commit

---
+++ gh copilot gpt5 photo renamer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gh copilot gpt5 photo renamer
+//
+//  Created by clive darra on 07/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
